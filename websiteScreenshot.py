b0da68254713464880e7dd7d12d699351bee7730
Ronan Cremin <rcremin@afilias.info>
2015-11-13 14:09:53 +0000

    Added command line parameters
    Added a couple of command line parameters to make the script more
    flexible

--- websiteScreenshot.py
+++ websiteScreenshot.py
@@ -2,33 +2,47 @@ import urllib2
 import json
 import base64
 import sys
+import argparse
 
-#	The website's URL
-site = "https://twitter.com/edent"
 
-#	The Google API.  Remove "&strategy=mobile" for a desktop screenshot
-api = "https://www.googleapis.com/pagespeedonline/v1/runPagespeed?screenshot=true&strategy=mobile&url=" + urllib2.quote(site)
+def main(args):
+    site = args.url[0]
+    if not site.startswith('http://'):
+        site = 'http://' + site
 
-#	Get the results from Google
-try:
-    site_data = json.load(urllib2.urlopen(api))
-except urllib2.URLError:
-    print "Unable to retreive data"
-    sys.exit()
+    if args.desktop:
+        api = "https://www.googleapis.com/pagespeedonline/v1/runPagespeed?screenshot=true&url="
+    else:
+        api = "https://www.googleapis.com/pagespeedonline/v1/runPagespeed?screenshot=true&strategy=mobile&url="
 
-try:
-    screenshot_encoded =  site_data['screenshot']['data']
-except ValueError:
-    print "Invalid JSON encountered."
-    sys.exit()	
+    api += urllib2.quote(site)
 
-#	Google has a weird way of encoding the Base64 data
-screenshot_encoded = screenshot_encoded.replace("_", "/")
-screenshot_encoded = screenshot_encoded.replace("-", "+")
+    try:
+        site_data = json.load(urllib2.urlopen(api))
+    except urllib2.URLError:
+        print "Unable to retreive data"
+        sys.exit()
 
-#	Decode the Base64 data
-screenshot_decoded = base64.b64decode(screenshot_encoded)
+    try:
+        screenshot_encoded =  site_data['screenshot']['data']
+    except ValueError:
+        print "Invalid JSON encountered."
+        sys.exit()	
 
-#	Save the file
-with open('screenshot.jpg', 'w') as file_:
-    file_.write(screenshot_decoded)
+    #	Google has a weird way of encoding the Base64 data
+    screenshot_encoded = screenshot_encoded.replace("_", "/")
+    screenshot_encoded = screenshot_encoded.replace("-", "+")
+
+    #	Decode the Base64 data
+    screenshot_decoded = base64.b64decode(screenshot_encoded)
+
+    #	Save the file
+    with open('screenshot.jpg', 'w') as file_:
+        file_.write(screenshot_decoded)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Save a screenshot of a site using Google PageSpeed service.')
+    parser.add_argument('--url', '-u', nargs=1, required=True, help='The URL to create the screenshot from')
+    parser.add_argument('--desktop', default=False, action='store_true', required=False, help='Get screenshot of desktop experience')
+    main(parser.parse_args())
